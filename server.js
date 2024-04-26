906faf7965028892e8ae60db2fd70cf7f3933fc0
AdepuShashank <shahsank.adepu5@gmail.com>
2024-04-26 16:47:48 +0530

    synch hashing

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res)
+      console.log(res);
     });
   });
 
@@ -22,8 +23,10 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
